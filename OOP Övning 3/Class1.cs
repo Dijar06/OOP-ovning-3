f719201eec705e13ecc7c948328ac9c2165bb40b
Dijar06 <dize06001@utb.vaxjo.se>
2024-04-05 13:09:42 +0200

    Add project files.

---
+++ OOP Övning 3/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Övning_3
+{
+    class Class1
+    {
+        private string förnamn;
+        private string efternamn;
+        public int ålder;
+
+        public string metod1
+        {
+            get { return förnamn; }
+            set { förnamn = value; }
+        }
+
+        public string metod2
+        {
+            get { return efternamn; }
+            set { efternamn = value; }
+        }
+
+        public bool myndig()
+        {
+            return ålder >= 18;
+        }
+    }
+}
---
+++ OOP Övning 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP_Övning_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 p1 = new Class1();
+            p1.metod1 = "Dijar";
+            p1.metod2 = "Zejnullahi";
+
+            Console.WriteLine(p1.metod1 + " " + p1.metod2);
+
+            DateTime födelsedatum = new DateTime(2006, 12, 11);
+            Console.WriteLine(födelsedatum.ToString());
+
+            Console.WriteLine("Hur gammal är du?");
+            p1.ålder = int.Parse(Console.ReadLine());
+            Console.WriteLine("Myndig: " + p1.myndig());
+        }
+    }
+}
